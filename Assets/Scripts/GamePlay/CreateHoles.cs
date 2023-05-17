300e65979cb3b45eca543e2ab97bbe4bcfb1d2bf
lehoanghuy11062001 <lehoanghuy11062001@gmail.com>
2023-05-17 17:13:31 +0700

    init code

---
+++ Assets/Scripts/GamePlay/CreateHoles.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CreateHoles : MonoBehaviour
+{
+    public GameObject holePrefab;
+    public float jointDistance = 1f;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Instantiate(holePrefab, mousePosition, Quaternion.identity);
+
+            // Liên kết lỗ mới tạo với lỗ trước đó (nếu có)
+            ConnectHoles();
+        }
+    }
+
+    private void ConnectHoles()
+    {
+        GameObject[] holes = GameObject.FindGameObjectsWithTag("Hole");
+
+        if (holes.Length < 2)
+            return;
+
+        int lastIndex = holes.Length - 1;
+        var previousHole = holes[lastIndex - 1].GetComponent<Rigidbody2D>();
+        var currentHole = holes[lastIndex].GetComponent<Rigidbody2D>();
+
+        var distanceJoint = currentHole.gameObject.AddComponent<DistanceJoint2D>();
+        distanceJoint.connectedBody = previousHole;
+        distanceJoint.distance = jointDistance;
+    }
+}
